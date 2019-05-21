fdde8070ef26c7d8a3d9984838d766dc952e6ccb
Grzegorz <grzegorz.petrykowski@gmail.com>
2019-05-21 19:35:52 +0100

    Content updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Heal to Thrive',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Talking therapy with Hanna Grzesik',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
